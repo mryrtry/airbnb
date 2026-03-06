e6ae6b5fc58449d81ccdadc415a05f77df12a535
mryrtry <maks01-16@mail.ru>
2026-03-07 02:53:45 +0300

    fix(airbnb): fix type check violation

--- src/main/java/org/mryrt/airbnb/notification/model/Notification.java
+++ src/main/java/org/mryrt/airbnb/notification/model/Notification.java
@@ -32,7 +32,7 @@ public class Notification {
     private Long userId;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, length = 80, columnDefinition = "varchar(80) not null")
     private NotificationType type;
 
     @Column(nullable = false, length = 500)
