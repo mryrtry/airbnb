fc2a56947069400d9c0d712ca208ddbee577de50
mryrtry <maks01-16@mail.ru>
2026-03-07 03:18:38 +0300

    fix: optional mail config for prod

--- src/main/java/org/mryrt/airbnb/notification/service/email/SmtpEmailService.java
+++ src/main/java/org/mryrt/airbnb/notification/service/email/SmtpEmailService.java
@@ -1,7 +1,7 @@
 package org.mryrt.airbnb.notification.service.email;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,10 +9,10 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class SmtpEmailService implements EmailService {
 
-    private final JavaMailSender mailSender;
+    @Autowired(required = false)
+    private JavaMailSender mailSender;
 
     @Value("${airbnb.mail.from:airbnb@localhost}")
     private String fromAddress;
@@ -23,8 +23,8 @@ public class SmtpEmailService implements EmailService {
     @Override
     public void send(String to, String subject, String body) {
         if (to == null || to.isBlank()) return;
-        if (!enabled) {
-            log.debug("Mail disabled, skip send to {}: {}", to, subject);
+        if (!enabled || mailSender == null) {
+            log.debug("Mail disabled or not configured, skip send to {}: {}", to, subject);
             return;
         }
         try {
